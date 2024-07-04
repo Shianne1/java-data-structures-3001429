002ff47713f6534c47fb39b9082bbe34ef153bd9
Shianne <slesure1@ggc.edu>
2024-07-04 16:22:36 -0400

    Deque Stack
    Replace the top number of a stack

--- src/main/java/StackExamples.java
+++ src/main/java/StackExamples.java
@@ -16,6 +16,38 @@ public class StackExamples {
     stack.poll();
     System.out.println(stack);
 
+
+
+    // Replace the top number of a stack
+    Deque<Integer> stack1 = new ArrayDeque<>();
+    stack1.push(2);
+    stack1.push(3);
+    stack1.push(5);
+    stack1.push(7);
+    stack1.push(10);
+
+    int newTop = 11;
+    Deque<Integer> result = Answer.updateStack(stack1, newTop);
+    System.out.println(result);
+  }
+
+}
+
+
+
+class Answer {
+
+  // Change these boolean values to control whether you see
+  // the expected result and/or hints.
+  static boolean showExpectedResult = false;
+  static boolean showHints = false;
+
+  // Return the same Stack as given in the input, but with the top number replaced by newTop.
+  static Deque<Integer> updateStack(Deque<Integer> stack, int newTop) {
+    // Your code goes here.
+    stack.pop();
+    stack.push(newTop);
+    return stack;
   }
 
 }
