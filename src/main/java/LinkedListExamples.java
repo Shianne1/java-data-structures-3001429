2c44a943f488701967051b4d28ba2906deb34d14
Shianne <slesure1@ggc.edu>
2024-07-04 15:30:12 -0400

    Linked list
    add an item at given position in a linked list

--- src/main/java/LinkedListExamples.java
+++ src/main/java/LinkedListExamples.java
@@ -1,6 +1,7 @@
 import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Arrays;
 
 public class LinkedListExamples {
 
@@ -20,7 +21,33 @@ public class LinkedListExamples {
 
     List<String> synchronizedShoppingList = Collections.synchronizedList(shoppingList);
     System.out.println(synchronizedShoppingList);
-    
+
+
+
+    // add an item at given position in a linked list
+    LinkedList<String> words = new LinkedList<>(Arrays.asList("Mercury", "Venus", "Mars"));
+    //LinkedList<String> wordsCopy = new LinkedList<>(words);
+    String newWord = "Earth";
+    int index = 2;
+    LinkedList<String> result = Answer.updateLinkedList(words, newWord, index);
+    System.out.println(result);
+  }
+
+}
+
+
+class Answer {
+
+  // Change these boolean values to control whether you see
+  // the expected result and/or hints.
+  static boolean showExpectedResult = false;
+  static boolean showHints = false;
+
+  // Add the newWord to the words list at the given index
+  static LinkedList<String> updateLinkedList(LinkedList<String> words, String newWord, int index) {
+    // Your code goes here.
+    words.add(index, newWord);
+    return words;
   }
 
 }
