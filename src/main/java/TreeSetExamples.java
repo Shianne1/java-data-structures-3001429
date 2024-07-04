3f33414cc345ccda97c9029489213416323b90c6
Shianne <slesure1@ggc.edu>
2024-07-04 17:27:35 -0400

    Tree Set
    Add the given strings to a tree Set and order by length

--- src/main/java/TreeSetExamples.java
+++ src/main/java/TreeSetExamples.java
@@ -20,11 +20,44 @@ public class TreeSetExamples {
     wordSet.add("giraffe");
     wordSet.add("bear");
     System.out.println(wordSet);
-    wordSet.add("wolf");
+    wordSet.add("wolf"); // when comparing by length, it sees wolf as a duplicate therefore, will not add it
     System.out.println(wordSet);
     wordSet.remove("giraffe");
     System.out.println(wordSet);
 
+
+
+    // Add the given strings to a treeSet and order by length
+    String word1 = "red";
+    String word2 = "yellow";
+    String word3 = "blue";
+
+    TreeSet<String> result = Answer.createSortedTreeSet(word1, word2, word3);
+    System.out.println(result);
+  }
+
+}
+
+
+
+class Answer {
+
+  // Change these boolean values to control whether you see
+  // the expected result and/or hints.
+  static boolean showExpectedResult = true;
+  static boolean showHints = true;
+
+  // Return a TreeSet containing the three words, ordered by length.
+  static TreeSet<String> createSortedTreeSet(String word1, String word2, String word3) {
+    // Your code goes here.
+    TreeSet<String> lengthSet = new TreeSet<>(Comparator.comparing(String::length));
+    lengthSet.add(word1);
+    lengthSet.add(word2);
+    lengthSet.add(word3);
+
+    return lengthSet;
+
+    //return new TreeSet<>(lengthSet.);
   }
 
 }
